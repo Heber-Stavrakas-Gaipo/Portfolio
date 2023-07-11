acb010212f547adf7a82afe8e027e8f3d4c9bcc8
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-07-10 22:23:20 -0300

    v2.0.0 increase theme switch and jump effect on projects

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 This initial project will be used to exercite HTML and CSS principally, as the knowlegede progresses I will introduce others concepts, technologies and languages.
 
-## What this one have now:
+## What does this one have now:
 - HTML
 - CSS
 - JavaScript
     - Axios
+
+## What source do we have:
+- API request
+- Theme switch
--- main.js
+++ main.js
@@ -54,3 +54,29 @@ function getLanguage() {
         .catch(e)
 }
 getLanguage()
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-light') {
+        setTheme('theme-default');
+    } else {
+        setTheme('theme-light');
+    }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-default');
+      document.getElementById('slider').checked = true;
+    }
+})();
