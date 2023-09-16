3717237627acbc29f4827fccc3f4db5449ac7ae2
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-09-16 17:47:37 -0300

    render projects from back-end

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-const express = require("express")
-const favicon = require('serve-favicon')
-const path = require('path')
-const app = express()
+const express = require("express");
+const favicon = require("serve-favicon");
+const path = require("path");
+const app = express();
+const url = "https://api.github.com/users/Heber-Stavrakas-Gaipo/repos";
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/assets'));
-app.use(favicon(path.join(__dirname, 'assets', 'icons', 'myfavicon.png')));
+app.use(express.static(__dirname + "/assets"));
+app.use(favicon(path.join(__dirname, "assets", "icons", "myfavicon.png")));
 
-app.get("/", function (req, res) {
-    res.render("pages/home/index")
-})
+app.get("/", async function (req, res) {
+  const postsResponse = fetch(url);
+  const [posts] = await Promise.all([postsResponse]);
+  const postsJson = await posts.json();
+  // Aplicar a lógica para ignorar o objeto com name igual a "Héber"
+  const filteredPosts = postsJson.filter((post) => post.name !== "Heber-Stavrakas-Gaipo");
+  const post = filteredPosts.map((post) => {
+    return {...post};
+  });
+  post.sort((a, b) => a.id - b.id);
+  res.render("pages/home/index", {projects: post});
+});
 
 app.get("/about", function (req, res) {
-    res.render("pages/about/about")
-})
+  res.render("pages/about/about");
+});
 
 app.get("/certificates", function (req, res) {
-    res.render("pages/certificates/certificates")
-})
+  res.render("pages/certificates/certificates");
+});
 
 app.listen(2023);
-console.log("Rodando!")
+console.log("Rodando!");
